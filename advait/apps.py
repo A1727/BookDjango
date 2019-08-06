2cfe8b5fadb9a1623757458391a21a9e774ccc72
Advait Sathe <advait.sathe@gmail.com>
2019-08-06 18:28:14 +0530

    Made self profile and also made farewell profile.

---
+++ advait/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdvaitConfig(AppConfig):
+    name = 'advait'
---
+++ advait/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def all(request):
+    return render(request, "all.html")
